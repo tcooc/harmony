791f0d14e5a2ba59b8ef2625a19204e6caf255ef
tcooc <tcooc@users.noreply.github.com>
2015-11-18 20:47:04 -0500

    rename secrets variable.

--- secrets.js
+++ secrets.js
@@ -1,6 +1,6 @@
 var fs = require('fs');
 
-var secretsFile = process.env['SECRETS'];
+var secretsFile = process.env.HARMONY_SECRETS;
 var secrets = JSON.parse(fs.readFileSync(secretsFile).toString());
 
 module.exports = secrets;
