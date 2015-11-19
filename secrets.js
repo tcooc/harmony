066c2cac34abc771b4122d335d54893880260f4f
tcooc <tcooc@users.noreply.github.com>
2015-11-18 20:48:32 -0500

    log secrets

--- secrets.js
+++ secrets.js
@@ -1,6 +1,7 @@
 var fs = require('fs');
 
 var secretsFile = process.env.HARMONY_SECRETS;
+console.log('Loading secrets from ' + secretsFile);
 var secrets = JSON.parse(fs.readFileSync(secretsFile).toString());
 
 module.exports = secrets;
