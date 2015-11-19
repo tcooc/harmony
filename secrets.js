4e55301a0a67259ccb1a7f91199e03e58dfd107a
tcooc <tcooc@users.noreply.github.com>
2015-11-18 21:08:29 -0500

    forever doesn't accept anything

--- secrets.js
+++ secrets.js
@@ -1,7 +1,5 @@
 var fs = require('fs');
 
-var secretsFile = process.env.HARMONY_SECRETS;
-console.log('Loading secrets from ' + secretsFile);
-var secrets = JSON.parse(fs.readFileSync(secretsFile).toString());
+var secrets = JSON.parse(fs.readFileSync('.secrets').toString());
 
 module.exports = secrets;
