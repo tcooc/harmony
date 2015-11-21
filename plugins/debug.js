0d75aad11a0d5312efca8cdf572ce1bcb6cf284d
tcooc <tcooc@users.noreply.github.com>
2015-11-20 22:01:53 -0500

    log userid

--- plugins/debug.js
+++ plugins/debug.js
@@ -1,5 +1,7 @@
 module.exports = function(messaging) {
 	messaging.addHook(function(message) {
-		console.log(message.author.username, message.channel.name + '(' + message.channel.id + ')', message.content);
+		console.log(message.author.username + '(' + message.author.id + ')',
+			message.channel.name + '(' + message.channel.id + ')',
+			message.content);
 	});
 };
