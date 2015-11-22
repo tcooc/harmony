187eeefe21c8feb0c40ceff2cd2b46ae2af214b3
tcooc <tcooc@users.noreply.github.com>
2015-11-22 02:38:17 -0500

    Debug twitter issues.

--- plugins/twitter.js
+++ plugins/twitter.js
@@ -1,7 +1,6 @@
 var _ = require('underscore');
 var Twitter = require('twitter');
 var twitter = require('../lib/twitter');
-var secrets = require('../secrets');
 
 function createTwitterPlugin(twitterKeys, twitterId, channelIds, accept) {
 	var twitterClient = new Twitter(twitterKeys);
@@ -9,9 +8,11 @@ function createTwitterPlugin(twitterKeys, twitterId, channelIds, accept) {
 		var channels = _.map(channelIds, function(id) {
 			return messaging.client.channels.get('id', id);
 		});
+		console.log(channels);
 		twitter.createStream(twitterClient, twitterId).then(function(stream) {
 			stream.on('data', function(tweet) {
 				console.log('Tweet:' + tweet.text);
+				console.log(tweet.user.id_str, tweet.retweeted_status, tweet.text, accept);
 				if(tweet.user.id_str === twitterId && !tweet.retweeted_status && accept.test(tweet.text)) {
 					_.each(channels, function(channel) {
 						messaging.client.sendMessage(channel, tweet.text);
