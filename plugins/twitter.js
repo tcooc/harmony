9a676ea39522790f3817042bba6f75d9620dcbca
tcooc <tcooc@users.noreply.github.com>
2015-11-30 14:21:27 -0500

    Magical update logic.

--- plugins/twitter.js
+++ plugins/twitter.js
@@ -5,6 +5,8 @@ var winston = require('winston');
 var twitter = require('lib/twitter');
 
 var ALERT_TWEET_REGEX = /- ([0-9]+)m -/;
+var SECOND = 1000;
+var MINUTE = 60 * SECOND;
 
 function createTwitterPlugin(twitterFollow, twitterBroadcasts) {
 	var plugin = {
@@ -16,6 +18,66 @@ function createTwitterPlugin(twitterFollow, twitterBroadcasts) {
 		plugin.client = new Twitter(messaging.settings.twitter);
 
 		var broadcasts = [];
+
+		function cleanup(channels, amount) {
+			var promise = Promise.resolve();
+			_.each(channels, function(channel) {
+				promise = promise.then(function() {
+					return client.getChannelLogs(channel, amount);
+				})
+				.then(function(messages) {
+					return _.filter(messages, function(message) {
+						return message.author.id === client.user.id;
+					});
+				})
+				.then(function(messages) {
+					return Promise.all(_.map(messages, function(message) {
+						var match = ALERT_TWEET_REGEX.exec(message.content);
+						if(match) {
+							var duration = (+match[1]) * MINUTE;
+							var timestamp = message.editedTimestamp || message.timestamp;
+							if(Date.now() - timestamp > duration) {
+								return client.deleteMessage(message);
+							}
+						}
+					}));
+				});
+			});
+			return promise;
+		}
+
+		function updateAlertMessage(message) {
+			var duration = (+ALERT_TWEET_REGEX.exec(message.content)[1]) * MINUTE;
+			var expiresAt = message.timestamp + duration;
+			var expiresIn = (expiresAt - Date.now()) / MINUTE;
+			if(expiresIn <= 0) {
+				client.deleteMessage(message);
+			} else {
+				var content = message.content.replace(ALERT_TWEET_REGEX, '- ' + Math.round(expiresIn)  + 'm -');
+				if(content !== message.content) {
+					client.updateMessage(message, content);
+				}
+				setTimeout(updateAlertMessage.bind(null, message), 10 * SECOND);
+			}
+		}
+
+		function handleTweet(tweet) {
+			winston.info('Tweet: ' + tweet.text, tweet.user.id_str, !tweet.retweeted_status);
+			if(tweet.user.id_str === twitterFollow && !tweet.retweeted_status) {
+				_.each(broadcasts, function(broadcast) {
+					if(broadcast.accept.test(tweet.text)) {
+						var broadcastPromise = messaging.broadcast(broadcast.channels, tweet.text);
+						if(ALERT_TWEET_REGEX.test(tweet.text)) {
+							broadcastPromise.then(function(results) {
+								_.each(results, function(message) {
+									updateAlertMessage(message);
+								});
+							});
+						}
+					}
+				});
+			}
+		}
 
 		client.on('ready', function() {
 			_.each(twitterBroadcasts, function(twitterBroadcast) {
@@ -39,16 +101,7 @@ function createTwitterPlugin(twitterFollow, twitterBroadcasts) {
 			return stream;
 		})
 		.then(function(stream) {
-			stream.on('data', function(tweet) {
-				winston.info('Tweet:' + tweet.text, tweet.user.id_str, !!tweet.retweeted_status);
-				if(tweet.user.id_str === twitterFollow && !tweet.retweeted_status) {
-					_.each(broadcasts, function(broadcast) {
-						if(broadcast.accept.test(tweet.text)) {
-							messaging.broadcast(broadcast.channels, tweet.text);
-						}
-					});
-				}
-			});
+			stream.on('data', handleTweet);
 			stream.on('error', function(error) {
 				winston.error('Twitter error: ' + error.source);
 				throw error;
@@ -57,33 +110,6 @@ function createTwitterPlugin(twitterFollow, twitterBroadcasts) {
 			return stream;
 		});
 
-		function cleanup(channels, amount) {
-			var promise = Promise.resolve();
-			_.each(channels, function(channel) {
-				promise = promise.then(function() {
-					return client.getChannelLogs(channel, amount);
-				})
-				.then(function(messages) {
-					return _.filter(messages, function(message) {
-						return message.author.id === client.user.id;
-					});
-				})
-				.then(function(messages) {
-					return Promise.all(_.map(messages, function(message) {
-						var match = ALERT_TWEET_REGEX.exec(message.content);
-						if(match) {
-							var duration = (+match[1]) * 60 * 1000;
-							var timestamp = message.timestamp;
-							if(Date.now() - timestamp > duration) {
-								return client.deleteMessage(message);
-							}
-						}
-					}));
-				});
-			});
-			return promise;
-		}
-
 		messaging.addCommandHandler(/^twitter:clean/i, function(message, content) {
 			if(message.author.id !== messaging.settings.owner) {
 				return;
@@ -91,7 +117,7 @@ function createTwitterPlugin(twitterFollow, twitterBroadcasts) {
 			var index = content.length > 1 ? +content[1] : 0;
 			cleanup(broadcasts[index].channels, 500)
 			.then(function() {
-				client.sendMessage(message.channel, 'finished cleaning ' + index);
+				client.sendMessage(message.channel, 'finished cleaning stream ' + index);
 			});
 			return true;
 		});
