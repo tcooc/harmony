cbf4bd41079f750fe53a9f3809d4f2e9049344a5
tcooc <tcooc@users.noreply.github.com>
2015-12-03 15:05:14 -0500

    !about command.

--- plugins/help.js
+++ plugins/help.js
@@ -7,7 +7,7 @@ var commands = [
 	'!wiki', 'Get wiki link for the specified topic. For example, `!wiki excalibur`',
 	'!trialstats', 'Get links to trial stats',
 	'!alertme', 'Personalized alert notifications. Type `!alertme` to see instructions',
-	'!invite', 'PM me `!invite <Discord invite url>` to invite my to your server',
+	'!invite', 'PM me `!invite <Discord invite url>` to invite me to your server',
 	'!flip', 'Flip your table',
 	'!unflip', 'Unflip a table',
 	'soon', '(coming soon)'
@@ -16,12 +16,19 @@ var commands = [
 function generateHelpMessage() {
 	var message = [];
 	for(var i = 0; i < commands.length; i += 2) {
-		message.push(commands[i] + ' - ' + commands[i + 1]);
+		message.push('**' + commands[i] + '** - ' + commands[i + 1]);
 	}
 	return message.join('\n');
 }
 
 module.exports = function(messaging, client) {
+	messaging.addCommandHandler(/^!about?/i, function(message) {
+		client.sendMessage(message.author, 'I am a bot created by tcooc for Warframe related matters.\n' +
+			'Type `!commands` to see what I can do.\n' +
+			'Feel free to PM my creator if you have any feedback.');
+		return true;
+	});
+
 	messaging.addCommandHandler(/^!commands?/i, function(message) {
 		client.sendMessage(message.author, generateHelpMessage());
 		return true;
