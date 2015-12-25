b53af128e273dfa25f3df9b5a251cdd8db0c9b8e
tcooc <tcooc@users.noreply.github.com>
2015-12-25 01:36:58 -0500

    2.0 is coming

--- plugins/discord.js
+++ plugins/discord.js
@@ -2,10 +2,10 @@ var Discord = require('discord.js');
 
 module.exports = function(messaging, client) {
 	messaging.addCommandHandler(/^!invite/i, function(message, content) {
-		if(content[1].indexOf('discord.gg') > -1 && message.channel instanceof Discord.PMChannel) {
+		if((message.channel instanceof Discord.PMChannel) {
 			client.joinServer(content[1], function(err, server) {
 				if(err) {
-					client.sendMessage(message.channel, 'Something went wrong, please contact admins');
+					client.sendMessage(message.channel, 'Something is wrong with the invite url, please try again');
 				} else {
 					client.sendMessage(message.channel, 'Joined ' + server.name + ' successfully');
 				}
